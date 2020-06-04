8a781e787b1730448233a5cc2f9c97de509a9d46
falexandrou97 <falexandrou@uclan.ac.uk>
2020-06-04 13:39:12 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+nltk.download()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/tokenize/<sentence>', methods=['GET'])
+def get_tokens(sentence):
+    # Get Tokens
+    tokens = word_tokenize(sentence)
+    # Lemmatize Tokens
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    # Pos-Tagging
+    pos_tagged = nltk.pos_tag(lemmatized_tokens)
+    result = []
+    for word, tag in pos_tagged:
+        if tag not in ['DT', 'IN', 'PRP', 'CC']:
+            result.append(word)
+
+    response = {
+        'query': sentence,
+        'tokens': result
+    }
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
